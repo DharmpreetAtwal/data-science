198aeb75a1205b7ec962bbad4cacd5e150adb6ab
Dharmpreet Atwal <dharmpreet1.atwal@protonmail.com>
2024-12-17 14:58:48 -0500

    Added Cluster Visual, fixed df_clean

--- travel/travel_script.py
+++ travel/travel_script.py
@@ -12,6 +12,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.feature_selection import RFECV
 from sklearn.model_selection import GridSearchCV
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import PCA
 
 from imblearn.under_sampling import RandomUnderSampler
 
@@ -49,23 +51,21 @@ low_cardinal_cat = ['site_name', 'posa_continent', 'user_location_country', 'cha
 df_features = pd.get_dummies(df_features, columns=low_cardinal_cat)
 
 # Apply Frequency Encoding to high cardinality categorical data
+# Replace high cardinality features with their frequency encoded versions
 high_cardinal_cat = ['user_location_region', 'user_location_city', 'srch_destination_id', 'hotel_country', 'hotel_market', 'hotel_cluster']
 for feature in high_cardinal_cat:
     freq_encoding = df_features[feature].value_counts(normalize=True)
     df_features[feature + "_Encoded"] = df_features[feature].map(freq_encoding)
-
-# Replace high cardinality features with their frequency encoded versions
 df_features.drop(high_cardinal_cat, inplace=True, axis=1)
 
 df_labels = df_clean['is_booking'].reset_index(drop=True)
-
 
 # %%
 # Use RandomUnderSampler to create a artificial dataset with no class imbalance
 rus = RandomUnderSampler(random_state=42)
 X, y = rus.fit_resample(df_features, df_labels)
 
-# Perform feature selection on this artificial dataset 
+# Perform feature selection on this artificial dataset using Random Forest
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 rf = RandomForestClassifier(random_state=42)
 rf.fit(X_train, y_train)
@@ -78,7 +78,7 @@ disp.plot()
 plt.show()
 
 # %%
-# Display the importance of each faeture
+# Display the 20 most important features
 f_i = list(zip(X_train.columns, rf.feature_importances_))
 f_i.sort(key=lambda x: x[1], reverse=True)
 
@@ -123,34 +123,35 @@ disp.plot()
 plt.show()
 
 # %%
+df_clean = df_clean.sample(5000)
 
-# =============================================================================
-# kmeans = KMeans(n_clusters=2, random_state=42)
-# pred_labels = kmeans.fit_predict(df_features_reduced, df_labels)
-# 
-# pca = PCA(n_components=2)
-# pca_components = pca.fit_transform(df_features_reduced)
-# 
-# m = ['o', 'x']
-# c = ['hotpink', 'red']
-# 
-# 
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# 
-# for i in range(len(pca_components)):
-#     if df_labels.at[i] == 0:
-#         marker_index = 0
-#     else:
-#         marker_index = 1
-# 
-#     if marker_index != 0:
-#         continue
-# 
-#     ax.scatter(pca_components[i][0], pca_components[i][1],
-#                 color=c[pred_labels[i]],
-#                 marker=m[marker_index])
-# =============================================================================
+
+
+kmeans = KMeans(n_clusters=2, random_state=42)
+pred_labels = kmeans.fit_predict(df_features_reduced, df_labels)
+
+pca = PCA(n_components=2)
+pca_components = pca.fit_transform(df_features_reduced)
+
+m = ['o', 'x']
+c = ['hotpink', 'red']
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+for i in range(len(pca_components)):
+    if df_labels.at[i] == 0:
+        marker_index = 0
+    else:
+        marker_index = 1
+
+    #if marker_index != 0:
+    #    continue
+
+    ax.scatter(pca_components[i][0], pca_components[i][1],
+                color=c[pred_labels[i]],
+                marker=m[marker_index])
+
 
 
         
