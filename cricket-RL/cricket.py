d9479a2e97364335dabb46c9863b8cf855ce02d4
Dharmpreet Atwal <dharmpreet1.atwal@protonmail.com>
2025-01-05 21:21:28 -0500

    Added locate_area

--- cricket-RL/cricket.py
+++ cricket-RL/cricket.py
@@ -13,38 +13,43 @@ import cv2
 from PIL import Image
 # %%
 
-time.sleep(1)
-play_area = pyautogui.locateOnScreen('play_area.png', confidence=0.9)
-capture_area = Box(play_area.left, play_area.top, play_area.width, play_area.height * 0.71)
+time.sleep(2)
 
-image = pyautogui.screenshot(
+# Use pre-determined menu identifier to determine play/capture area
+# Play represents area where mouse can move
+# Capture represents area where the screenshot fed into the model is taken
+locate_area = pyautogui.locateOnScreen('locate_menu.png', confidence=0.95)
+capture_area = Box(locate_area.left, locate_area.top, locate_area.width, locate_area.height * 1.7)
+play_area = Box(locate_area.left, locate_area.top, locate_area.width, locate_area.height * 2.3)
+
+play_ss = pyautogui.screenshot(
     region=(int(play_area.left), 
             int(play_area.top), 
             int(play_area.width), 
             int(play_area.height)))
-image.show()
+play_ss.show()
 
-image2 = pyautogui.screenshot(
+capture_ss = pyautogui.screenshot(
     region=(int(capture_area.left), 
             int(capture_area.top), 
             int(capture_area.width), 
             int(capture_area.height)))
-image2.show()
-
+capture_ss.show()
 
 # %%
-# Preprocessing, resize to lower resolution, conver to gray scale
+# Preprocessing, resize to lower resolution
 
 # Maintain aspect ratio, 5x less data
-ratio = image2.height / image2.width
-new_width = int(image2.width / 5)
+ratio = capture_ss.height / capture_ss.width
+new_width = int(capture_ss.width / 5)
 new_height = int(new_width * ratio)
 
-# Convert PIL Image to NP array, Convert to BGR from RGB
-arr_image = np.array(image2)
+# Convert PIL Image to NP array
+arr_image = np.array(capture_ss)
 bgr = cv2.cvtColor(arr_image, cv2.COLOR_RGB2BGR)
 
+# Convert from RGB to BGR, then back to RGB
 resized_bgr = cv2.resize(bgr, (new_width, new_height), interpolation=cv2.INTER_NEAREST_EXACT)
 resized_rgb = cv2.cvtColor(resized_bgr, cv2.COLOR_BGR2RGB)
-resized_pil = Image.fromarray(resized_rgb).convert('L')
+resized_pil = Image.fromarray(resized_rgb)
 resized_pil.show()
